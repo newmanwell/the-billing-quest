de502c0855fb5df504c33e6dae784a9a08d78ec0
Shaun Newman <shaun.newman.p@gmail.com>
2026-08-14 21:12:56 -0500

    chore: added createActiveCustomers function and seeded data into active_customer table

--- db/seed.cjs
+++ db/seed.cjs
@@ -1,4 +1,5 @@
 const client = require('./client.cjs');
+const { createActiveCustomers } = require('./active-customers.cjs');
 
 const deleteTables = async() => {
   try {
@@ -49,6 +50,11 @@ const syncAndSeed = async() => {
   console.log('Adding Tables');
   await addTables();
   console.log('Tables Added');
+
+  console.log('Adding Active Customers');
+  await createActiveCustomers('Active Customer 1', 'Jacksonville', 'Active Customer 1 description', '1/10/2026', '1/15/2026');
+  await createActiveCustomers('Active Customer 2', 'Laredo', 'Active Customer 2 description', '4/15/2026', '4/20/2026');
+  console.log('Active Customers Added');
 
   await client.end();
   console.log('Disconnected to the DB');
