32ec63733253c81be4784355102f84d3ba7f224f
Shaun Newman <shaun.newman.p@gmail.com>
2026-08-24 20:25:20 -0400

    feat: added edit button and EditActiveCustomer component pops up with edit is clicked

---
+++ src/EditActiveCustomer.tsx
@@ -0,0 +1,13 @@
+const EditActiveCustomer = () => {
+  return (
+    <>
+      <div className="modal-overlay">
+        <div className="modal-content">
+          <h2>Edit Customer</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EditActiveCustomer;
